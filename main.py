2b3263bcb0eb6ed4dae10c33c60f63abfaa31d0d
naahian <nahiandev@gmail.com>
2025-09-06 23:36:26 +0600

    change host and port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth
@@ -25,3 +26,8 @@ def startup():
 @app.get("/")
 def root():
     return {"message": "Welcome to the Leafify Backend"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
